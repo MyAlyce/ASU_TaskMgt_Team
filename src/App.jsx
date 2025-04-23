1a7f4b875094b1d4565d76101a443d454fe7adcd
ChiragSeth14 <135326086+ChiragSeth14@users.noreply.github.com>
2025-04-22 18:06:55 -0700

    Removed the bar

--- src/App.jsx
+++ src/App.jsx
@@ -40,15 +40,6 @@ function App() {
             {isLoggedIn ? (
                 <div className="container">
                     <Header />
-                    {/* Page switcher for demo to see all pages made */}
-                    <div style={{ marginBottom: '1rem' }}>
-                        <button onClick={() => setCurrentPage('main')}>Main Page</button>
-                        <button onClick={() => setCurrentPage('sleep')}>Sleep Page</button>
-                        <button onClick={() => setCurrentPage('exercise')}>Exercise Page</button>
-                        <button onClick={() => setCurrentPage('food')}>Food Page</button>
-                        <button onClick={() => setCurrentPage('mindfulness')}>Mindfulness Page</button>
-                    </div>
-                    {/* */}
                     <div className="page-view">
                         {renderPage()}
                     </div>
